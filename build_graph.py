d19f9c1416e66c8d835e8bbc3c5cb5a82e41a4a6
Skander Mejdi <mejdi.skander@gmail.com>
2018-01-16 22:37:42 +0100

    encoding / normalization

--- build_graph.py
+++ build_graph.py
@@ -1,6 +1,8 @@
 import csv
 
+global_id = 0
 from_cd = {}
+career_cd = {}
 field_cd = {}
 zip_cd = {}
 use = [
@@ -29,22 +31,46 @@ def encodeZip(value):
         zip_cd[value] = len(zip_cd)
     return zip_cd[value]
 
+def encodeCareer(value):
+    if not value in career_cd.keys():
+        career_cd[value] = len(career_cd)
+    return career_cd[value]
+
 def encodeField(value):
     if not value in field_cd.keys():
         field_cd[value] = len(field_cd)
     return field_cd[value]
+
+def normalizeIncome(value):
+    if value > 0 and value < 40000:
+        return 1
+    elif value < 65000:
+        return 2
+    return 3
+
+def setId(value):
+    return global_id
+
+vars_treatment = {
+    'from': encodeFrom,
+    'id': setId,
+    'income': normalizeIncome,
+    'zipcode': encodeZip,
+    "field": encodeField,
+    "career": encodeCareer
+}
 
 def readFile():
     keys = []
     data = []
     indexes = []
 
-    i = 0
+    global global_id
     with open('data.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             row = row[:-1]
-            if i == 0:
+            if global_id == 0:
                 j = 0
                 for key in row:
                     if any(key in elem for elem in use):
@@ -54,8 +80,15 @@ def readFile():
             else:
                 data.append({})
                 for j in indexes:
-                    data[i - 1].update({keys[j]: toInt(row[j])})
-            i += 1
+                    if row[j] != '' and keys[j] in vars_treatment.keys():
+                        data[global_id - 1].update({
+                            keys[j]: vars_treatment[keys[j]](row[j])
+                        })
+                    else:
+                        data[global_id - 1].update({
+                            keys[j]: toInt(row[j])
+                        })
+            global_id += 1
     print data[:5]
     return data
 
